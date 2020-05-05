40cd1a4ae5eee6c4341bf5d3f5bccbc59c87d5af
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-05 16:23:11 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -1,3 +1,17 @@
 def phrase
   phrase = "Hello World"
 end
+
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
